1459fef05f816fc51ba03f026092f429c74bc9e3
lahenkenius <lahenkenius@unomaha.edu>
2020-11-13 14:31:24 -0600

    update

--- FantasyFootball/views.py
+++ FantasyFootball/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import models
+from .models import Game
+from .models import Team
 
-# Create your views here.
+
+
+class GameListView(ListView):
+    model = Game
+    template_name = 'game_list.html'
+    ordering = ['-week_number']
+
+class TeamListView(ListView):
+    model = Team
+    template_name = 'team_list.html'
+    ordering = ['-wins']
